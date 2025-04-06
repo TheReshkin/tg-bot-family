1f6b45c79f07f6061ddd1aefcd3b3ab4692e3709
Resh <tereshkin.03@inbox.ru>
2025-04-06 19:33:53 +0300

    Инициализацтия телеграм бота

---
+++ go.mod
@@ -0,0 +1,8 @@
+module resh.local/tg-bot-family
+
+go 1.23.2
+
+require (
+	github.com/go-telegram/bot v1.14.1
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/joho/godotenv"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+// Send any text message to the bot after the bot has been started
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка при загрузке .env файла")
+	}
+
+	telegram_token := os.Getenv("TELEGRAM_TOKEN")
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	// 7561415536:AAGKDwqB1EqQWssZmBtWrIuSjo-bbaal6Ug
+	opts := []bot.Option{
+		bot.WithDefaultHandler(callbackHandler),
+		bot.WithDefaultHandler(defaultHandler),
+	}
+
+	b, err := bot.New(telegram_token, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
+}
+
+func callbackHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	// answering callback query first to let Telegram know that we received the callback query,
+	// and we're handling it. Otherwise, Telegram might retry sending the update repetitively
+	// as it thinks the callback query doesn't reach to our application. learn more by
+	// reading the footnote of the https://core.telegram.org/bots/api#callbackquery type.
+	b.AnswerCallbackQuery(ctx, &bot.AnswerCallbackQueryParams{
+		CallbackQueryID: update.CallbackQuery.ID,
+		ShowAlert:       false,
+	})
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.CallbackQuery.Message.Message.Chat.ID,
+		Text:   "You selected the button: " + update.CallbackQuery.Data,
+	})
+}
+
+func defaultHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	// Проверка, что update.Message не nil
+	if update.Message == nil {
+		log.Println("defaultHandler: update.Message или update.Message.Chat == nil, пропускаем обработку")
+		return
+	}
+
+	kb := &models.InlineKeyboardMarkup{
+		InlineKeyboard: [][]models.InlineKeyboardButton{
+			{
+				{Text: "Button 1", CallbackData: "button_1"},
+				{Text: "Button 2", CallbackData: "button_2"},
+			}, {
+				{Text: "Button 3", CallbackData: "button_3"},
+			},
+		},
+	}
+
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:      update.Message.Chat.ID,
+		Text:        "Нажми на кнопку",
+		ReplyMarkup: kb,
+	})
+}
